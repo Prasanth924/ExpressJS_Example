635b3f9efdf2a9312a3dd61a58ee15c1fda9f490
Prasanth <prasanth924@live.com>
2016-04-01 00:21:38 -0500

    Update message

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var PORT = process.env.PORT||3000;
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication,function(req,res){
-    res.send('About US!!');
+    res.send('This is my About you page using express');
 });
 
 //Expose a folder
